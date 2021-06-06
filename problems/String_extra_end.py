113527ee38efba82c5deeecb120870d2dd2fe94c
failedpeanut <failedpeanut@gmail.com>
2021-06-06 10:53:22 +0530

    string ends

--- problems/String_extra_end.py
+++ problems/String_extra_end.py
@@ -2,7 +2,8 @@
 
 
 
-Given a string, return a new string made of 3 copies of the last 2 chars of the original string. The string length will be at least 2.
+Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+The string length will be at least 2.
 
 
 extra_end('Hello') → 'lololo'
