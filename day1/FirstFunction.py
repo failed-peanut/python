75920074fc864d574515316c710ee849164c6b03
failedpeanut <failedpeanut@gmail.com>
2021-06-12 18:18:46 +0530

    day1

--- day1/FirstFunction.py
+++ day1/FirstFunction.py
@@ -1,4 +1,12 @@
+#This is how we write single line comment in python
+'''
+This is how we write multi lines
+comment in python
+'''
+#All functions should be defined with 'def' key word and functionName then parameter list if any
+# the ':' don't forget this!
 def myName(name):
     print(name)
 
+#calling the function
 myName("Failed Peanut")
